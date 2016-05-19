4ab3b68c22fd7b75cecef3442176204ef611618c
Stefano De Vuono <io@stefanodevuono.com>
2016-05-19 16:11:08 -0700

    promisify tests with watcher and module reloading

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,27 @@
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import faucet from 'faucet';
-import glob from 'glob';
+import glob from 'glob-promise';
 
 const $ = gulpLoadPlugins();
 const testGlob = ['*-spec.js'];
 const nonTestNonGulpfileGlob = '!(gulpfile*|*spec).js';
 
-gulp.task('tape', () => {
-  glob(nonTestNonGulpfileGlob, (err, files) => {
-    files.forEach( file => delete require.cache[require.resolve(`./${file}`)] );
+gulp.task('reload-modules', () => {
+  var reloadPromise = new Promise(resolve => {
+    glob(nonTestNonGulpfileGlob).then(files => {
+        files.forEach( file => delete require.cache[require.resolve(`./${file}`)] );
+      }).then(() => {
+        resolve();
+      });
   });
+
+  return reloadPromise;
+});
+
+
+gulp.task('tape', ['reload-modules'], () => {
+  console.log('about to start the tests');
 
   return gulp.src(testGlob, { read: true } )
     .pipe($.tape({ reporter: faucet() }));
